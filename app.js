268c70794646408abe7511204b5b93e20e1e6579
Rafael Redigolo <rafaelredigolo@Rafaels-Mac.local>
2016-07-02 14:39:42 -0300

    teste 3

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
+console.log('his')
   res.send('hello jenkins');
-  console.log('his')
+
 });
 
 app.listen(process.env.PORT || 5000);
